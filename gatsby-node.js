27336120a1a87dcb2d1426ad19f5899ddb7cddad
Mateusz Kocz <mateusz@kocz.pl>
2019-01-11 07:49:43 +0100

    change the blog path

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,8 +37,10 @@ exports.createPages = ({ graphql, actions }) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
       const next = index === 0 ? null : posts[index - 1].node
 
+      console.log(post.node.fields.slug)
+
       createPage({
-        path: post.node.fields.slug,
+        path: '/blog' + post.node.fields.slug,
         component: blogPost,
         context: {
           slug: post.node.fields.slug,
