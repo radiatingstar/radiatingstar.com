2b2e13d3ddf16a1faa69b8f056316f1322e98b09
Mateusz Kocz <mateusz@kocz.pl>
2019-01-19 13:41:50 +0100

    Remove readme's content and licece

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://radiatingstar.com/`,
     social: {
       twitter: `mateuszkocz`,
+      github: `mateuszkocz`
     },
   },
   plugins: [
@@ -51,20 +52,20 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-30216970-1`,
       },
     },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Radiating Star`,
+        short_name: `Radiating Star`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
+        background_color: `#ffa500`,
+        theme_color: `#ffa500`,
         display: `minimal-ui`,
-        icon: `content/assets/radiating-star-logo.png`,
+        icon: `content/assets/star-logo.png`,
       },
     },
     `gatsby-plugin-offline`,
