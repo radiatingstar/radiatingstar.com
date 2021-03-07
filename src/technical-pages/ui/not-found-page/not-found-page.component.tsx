90c41dd60c85d1380013bc12bed0a1538f049c9a
Mateusz Kocz <mateusz@kocz.pl>
2021-03-07 11:41:23 +0100

    Add the 404 page

--- src/technical-pages/ui/not-found-page/not-found-page.component.tsx
+++ src/technical-pages/ui/not-found-page/not-found-page.component.tsx
@@ -1,12 +1,10 @@
-import { Link } from "gatsby"
 import React, { FunctionComponent } from "react"
 import { SEO } from "../../../seo"
+import { NotFoundPageContent } from "../not-found-page-content/not-found-page-content.component"
 
 export const NotFoundPage: FunctionComponent = () => (
   <div>
     <SEO title="404: Not Found" />
-    <h1>There are so many starts in the universe!</h1>
-    <p>No wonder we were not able to find this one.</p>
-    <Link to={"/"}>Go back</Link>
+    <NotFoundPageContent />
   </div>
 )
