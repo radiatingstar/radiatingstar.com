19b87dc7a9450c900dfb3f66acfafd86aae4b230
Mateusz Kocz <mateusz@kocz.pl>
2021-03-07 11:44:13 +0100

    Add summary coverage reporter

--- jest.config.ts
+++ jest.config.ts
@@ -32,12 +32,7 @@ export default {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ["json", "text", "lcov", "clover", "text-summary"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
