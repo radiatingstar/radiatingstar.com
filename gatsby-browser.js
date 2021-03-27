6329a8424feb506e622c85ba05ac41a3f9f628b6
Mateusz Kocz <mateusz@kocz.pl>
2021-03-27 17:37:01 +0100

    Add global styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
+import "modern-normalize/modern-normalize.css"
 import "prismjs/themes/prism-okaidia.css"
 import "typeface-lato"
 import "typeface-ubuntu"
--- src/backbone/ui/core-layout/core-layout.component.tsx
+++ src/backbone/ui/core-layout/core-layout.component.tsx
@@ -1,8 +1,20 @@
 import React, { FunctionComponent, ReactElement } from "react"
+import { createGlobalStyle } from "styled-components"
 import { Logo } from "../../../branding"
 import { mainNavigation } from "../../main-navigation-items"
 import { Header } from "../header/header.component"
 import { MainNavigation } from "../main-navigation/main-navigation.component"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Lato', sans-serif;
+  }
+
+  h1,
+  h2 {
+    font-family: 'Ubuntu', sans-serif;
+  }
+`
 
 interface Properties {
   navigation?: ReactElement
@@ -14,6 +26,7 @@ export const CoreLayout: FunctionComponent<Properties> = ({
 }) => {
   return (
     <div>
+      <GlobalStyle />
       <Header logoSlot={<Logo />} navigationSlot={navigation} />
       <main>{children}</main>
     </div>
