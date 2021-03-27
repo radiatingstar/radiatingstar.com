b30e47ddb97d929060ffd1977b13997a51ce2f9a
Mateusz Kocz <mateusz@kocz.pl>
2021-03-27 16:54:58 +0100

    Define main navigation links

---
+++ src/backbone/main-navigation-items.ts
@@ -0,0 +1,23 @@
+import { AppPath } from "./values/app-path/app-path.value"
+import { NavigationItemName } from "./values/navigation-item-name/navigation-item-name.value"
+import { NavigationItem } from "./values/navigation-item/navigation-item.value"
+import { Navigation } from "./values/navigation/navigation.value"
+
+export const mainNavigation = Navigation.create(
+  NavigationItem.create({
+    name: NavigationItemName.from("Home"),
+    path: AppPath.from("/"),
+  }),
+  NavigationItem.create({
+    name: NavigationItemName.from("Blog"),
+    path: AppPath.from("/blog"),
+  }),
+  NavigationItem.create({
+    name: NavigationItemName.from("Projects"),
+    path: AppPath.from("/projects"),
+  }),
+  NavigationItem.create({
+    name: NavigationItemName.from("Contact"),
+    path: AppPath.from("/contact"),
+  })
+)
