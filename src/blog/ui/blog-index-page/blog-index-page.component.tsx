5a1729bf0838ff063ac9963cad428e03ba121535
Mateusz Kocz <mateusz@kocz.pl>
2021-03-27 17:10:06 +0100

    Use the core layout in the blog index page and remove Tailwind from the page

--- src/blog/ui/blog-index-page/blog-index-page.component.tsx
+++ src/blog/ui/blog-index-page/blog-index-page.component.tsx
@@ -2,18 +2,15 @@ import { Link, PageProps } from "gatsby"
 import React, { FunctionComponent } from "react"
 import { BlogIndexQuery } from "../../../../graphql-types"
 import { assertDefined } from "../../../assertions"
+import { CoreLayout } from "../../../backbone"
 import { SEO } from "../../../seo"
-import { BlogLayout } from "../blog-layout/blog-layout.component"
 
 export const BlogIndexPage: FunctionComponent<PageProps<BlogIndexQuery>> = ({
   data: { allPosts },
 }) => {
   return (
-    <BlogLayout>
-      <SEO
-        title="All posts"
-        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-      />
+    <CoreLayout>
+      <SEO title="All posts" />
       {allPosts.edges.map(({ node: { excerpt, fields, frontmatter } }) => {
         assertDefined(frontmatter)
         assertDefined(fields)
@@ -21,16 +18,14 @@ export const BlogIndexPage: FunctionComponent<PageProps<BlogIndexQuery>> = ({
         const title = frontmatter.title || fields.slug
         return (
           <div key={fields.slug} className="mt-6">
-            <h2 className="mb-2 text-2xl text-yellow-500 font-bold">
-              <Link className="shadow-none" to={"/blog" + fields.slug}>
-                {title}
-              </Link>
+            <h2>
+              <Link to={"/blog" + fields.slug}>{title}</Link>
             </h2>
             <small>{frontmatter.date}</small>
             <p dangerouslySetInnerHTML={{ __html: excerpt }} />
           </div>
         )
       })}
-    </BlogLayout>
+    </CoreLayout>
   )
 }
