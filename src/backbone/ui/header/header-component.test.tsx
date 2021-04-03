7f96ca4e008b026360425bf97a5036173cc32cad
Mateusz Kocz <mateusz@kocz.pl>
2021-04-03 23:18:13 +0200

    Shorten the header tests

--- src/backbone/ui/header/header-component.test.tsx
+++ src/backbone/ui/header/header-component.test.tsx
@@ -3,16 +3,14 @@ import { render } from "@testing-library/react"
 import { Header } from "./header.component"
 
 describe("Header component", () => {
-  describe("when passed a logo", () => {
-    it("should render it", () => {
-      const { getByText } = render(<Header logoSlot={<div>logo</div>} />)
-      expect(getByText("logo")).toBeInTheDocument()
-    })
-  })
-  describe("when passes a navigation", () => {
-    it("should render it", () => {
-      const { getByText } = render(<Header navigationSlot={<div>nav</div>} />)
-      expect(getByText("nav")).toBeInTheDocument()
-    })
-  })
+  describe.each([["logo" as const], ["navigation" as const]])(
+    "when passed a %s",
+    (itemName) => {
+      it("should render it", () => {
+        const properties = { [itemName + "Slot"]: <div>{itemName}</div> }
+        const { getByText } = render(<Header {...properties} />)
+        expect(getByText(itemName)).toBeInTheDocument()
+      })
+    }
+  )
 })
