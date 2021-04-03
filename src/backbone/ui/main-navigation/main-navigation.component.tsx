59a995f5621bab924ce1f8fc3a39a9b9c99e6e72
Mateusz Kocz <mateusz@kocz.pl>
2021-04-03 20:48:07 +0200

    Style the main navigation

--- src/backbone/ui/main-navigation/main-navigation.component.tsx
+++ src/backbone/ui/main-navigation/main-navigation.component.tsx
@@ -3,12 +3,49 @@ import { map, Option, toNullable } from "fp-ts/Option"
 import { pipe } from "fp-ts/pipeable"
 import { Link } from "gatsby"
 import React, { VoidFunctionComponent } from "react"
+import styled from "styled-components"
 import { assertDefined } from "../../../assertions"
 import { Navigation } from "../../values/navigation/navigation.value"
 
 interface Properties {
   navigation: Option<Navigation>
 }
+
+const NavigationList = styled.ul`
+  // Reset.
+  list-style: none;
+  padding-left: 0;
+
+  margin: 0;
+
+  display: flex;
+  gap: 0.5rem;
+
+  font-size: 1.3rem;
+
+  @media (min-width: 25rem) {
+    gap: 2rem;
+    margin: 0 2rem;
+  }
+`
+
+const NavigationLink = styled(Link)`
+  // Reset.
+  text-decoration: none;
+
+  //color: rgb(30, 30, 30);
+  color: #268bd2;
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: #ffa500;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`
 
 export const MainNavigation: VoidFunctionComponent<Properties> = ({
   navigation,
@@ -21,7 +58,7 @@ export const MainNavigation: VoidFunctionComponent<Properties> = ({
       map((items) => {
         return (
           <nav>
-            <ul>
+            <NavigationList>
               {items.map((item, index) => {
                 const path = toNullable(item.path)
                 const name = toNullable(item.name)
@@ -29,11 +66,13 @@ export const MainNavigation: VoidFunctionComponent<Properties> = ({
                 assertDefined(name)
                 return (
                   <li key={index}>
-                    <Link to={path.value}>{name.value}</Link>
+                    <NavigationLink to={path.value}>
+                      {name.value}
+                    </NavigationLink>
                   </li>
                 )
               })}
-            </ul>
+            </NavigationList>
           </nav>
         )
       })
