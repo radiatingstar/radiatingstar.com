cfaba2123d31b4993170c58c7877f4f2b126df1f
Mateusz Kocz <mateusz@kocz.pl>
2021-04-03 23:05:43 +0200

    Shorten the navigation item value tests

--- src/backbone/values/navigation-item/navigation-item-value.test.ts
+++ src/backbone/values/navigation-item/navigation-item-value.test.ts
@@ -7,19 +7,13 @@ describe("Navigation item value object", () => {
   const name = NavigationItemName.from("name")
   const path = AppPath.from("/path")
   describe("when creating", () => {
-    describe("with name missing", () => {
+    describe.each([
+      ["name", { name: none, path }],
+      ["path", { name, path: none }],
+      ["name and path", { name: none, path: none }],
+    ])("with %s missing", (_, parameters) => {
       it("should return none", () => {
-        expect(NavigationItem.create({ name: none, path })).toBeNone()
-      })
-    })
-    describe("with path missing", () => {
-      it("should return none", () => {
-        expect(NavigationItem.create({ name, path: none })).toBeNone()
-      })
-    })
-    describe("with name and path values missing", () => {
-      it("should return none", () => {
-        expect(NavigationItem.create({ name: none, path: none })).toBeNone()
+        expect(NavigationItem.create(parameters)).toBeNone()
       })
     })
     describe("with name and path provided", () => {
@@ -30,18 +24,14 @@ describe("Navigation item value object", () => {
       })
     })
   })
-  describe("when accessing name object", () => {
+  describe.each([
+    ["name" as const, NavigationItemName],
+    ["path" as const, AppPath],
+  ])("when accessing %s object", (key: "name" | "path", expectedInstance) => {
     it("should return a proper value", () => {
       const item = toNullable(NavigationItem.create({ name, path }))
       // @ts-expect-error Here it's guaranteed the item will exist.
-      expect(toNullable(item.name)).toBeInstanceOf(NavigationItemName)
-    })
-  })
-  describe("when accessing path object", () => {
-    it("should return a proper value", () => {
-      const item = toNullable(NavigationItem.create({ name, path }))
-      // @ts-expect-error Here it's guaranteed the item will exist.
-      expect(toNullable(item.path)).toBeInstanceOf(AppPath)
+      expect(toNullable(item[key as unknown])).toBeInstanceOf(expectedInstance)
     })
   })
 })
