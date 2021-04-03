10c9ba925748d2789a67c47a760e354d36bf8757
Mateusz Kocz <mateusz@kocz.pl>
2021-04-03 23:59:39 +0200

    Make the badges' background less attention grabbing

--- src/backbone/ui/core-layout/elements/tertiary-footer-content/tertiary-footer-content.component.tsx
+++ src/backbone/ui/core-layout/elements/tertiary-footer-content/tertiary-footer-content.component.tsx
@@ -4,13 +4,12 @@ import styled from "styled-components"
 const TertiaryFooterContainer = styled.aside`
   margin-top: 3rem;
   padding: 0.5rem;
-  border: 1px solid black;
 
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.5rem;
 
-  background: white;
+  background: black;
   border-radius: 4px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
 
