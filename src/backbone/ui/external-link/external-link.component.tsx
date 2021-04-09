e17a02e00ee90d8870f4f33f4e3a30721ccaddcd
Mateusz Kocz <mateusz@kocz.pl>
2021-04-09 20:59:59 +0200

    Add documentation to the ExternalLink component

--- src/backbone/ui/external-link/external-link.component.tsx
+++ src/backbone/ui/external-link/external-link.component.tsx
@@ -8,6 +8,21 @@ interface Properties {
   href: string
 }
 
+/**
+ * Component to render a safe external link. Will handle the noopener, noreferrer
+ * and nofollow stuff for you.
+ *
+ * By default, noopener and noreferrer are set on the link.
+ *
+ * @example
+ * <ExternalLink href="https://nasa.gov" nofollow>Go to space!</ExternalLink>
+ *
+ * @param href The target of the link.
+ * @param children Link's content.
+ * @param noopener Should the link be noopener? Yes by default.
+ * @param noreferrer Should the link be noreferrer? Yes by default.
+ * @param nofollow Should the link be nofollow? No by default.
+ */
 export const ExternalLink: FunctionComponent<Stylable<Properties>> = ({
   href,
   children,
@@ -17,7 +32,7 @@ export const ExternalLink: FunctionComponent<Stylable<Properties>> = ({
   className,
 }) => {
   const relationValues = Object.entries({ noopener, noreferrer, nofollow })
-    .filter(([key, value]) => value)
+    .filter(([, value]) => value)
     .map(([key]) => key)
     .join(" ")
   return (
