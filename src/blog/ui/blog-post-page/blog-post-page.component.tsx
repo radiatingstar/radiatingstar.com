2b58abde0dde1d60fbf972ab83e9045a6be9277a
Mateusz Kocz <mateusz@kocz.pl>
2021-04-11 15:55:45 +0200

    Style the blog post

--- src/blog/ui/blog-post-page/blog-post-page.component.tsx
+++ src/blog/ui/blog-post-page/blog-post-page.component.tsx
@@ -4,6 +4,7 @@ import React, {
   JSXElementConstructor,
   PropsWithChildren,
 } from "react"
+import styled from "styled-components"
 import { BlogPostQuery, SitePageContext } from "../../../../graphql-types"
 import { assertDefined } from "../../../assertions"
 import { CoreLayout } from "../../../backbone"
@@ -33,12 +34,65 @@ export const BlogPostPage: FunctionComponent<Properties> = ({
   return (
     <Layout>
       <SEO title={post.frontmatter.title} description={post.excerpt} />
-      <h1>{post.frontmatter.title}</h1>
-      <p>
-        <span>{post.frontmatter.date}</span>, by <span>{author}</span>
-      </p>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <PostsNavigation {...pageContext} />
+      <Header>
+        <Title>{post.frontmatter.title}</Title>
+        <Info>
+          <span>{post.frontmatter.date}</span>, by <span>{author}</span>
+        </Info>
+      </Header>
+      <Content dangerouslySetInnerHTML={{ __html: post.html }} />
+      <footer>
+        <PostsNavigation {...pageContext} />
+      </footer>
     </Layout>
   )
 }
+
+const Header = styled.header`
+  padding: 1rem 1rem 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  text-align: center;
+  color: var(--yellow-500);
+
+  @media (min-width: 32rem) {
+    padding: 4rem;
+    gap: 4rem;
+  }
+`
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 0 2rem;
+  font-size: 3rem;
+
+  @media (min-width: 32rem) {
+    font-size: 4rem;
+  }
+`
+
+const Info = styled.aside``
+
+const Content = styled.section`
+  padding: 2rem;
+  background: white;
+
+  font-size: 1.2rem;
+  line-height: 1.7;
+
+  @media (min-width: 32rem) {
+    padding: 4rem;
+  }
+
+  p:first-child {
+    margin-top: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--yellow-400);
+  }
+`
