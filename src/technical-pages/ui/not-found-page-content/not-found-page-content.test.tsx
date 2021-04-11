e1172a44a806e27cd5784df9aae2b417481ccede
Mateusz Kocz <mateusz@kocz.pl>
2021-04-11 21:49:21 +0200

    Remove some maintanability issues from the 404 page tests

--- src/technical-pages/ui/not-found-page-content/not-found-page-content.test.tsx
+++ src/technical-pages/ui/not-found-page-content/not-found-page-content.test.tsx
@@ -1,38 +1,21 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import React from "react"
 import { NotFoundPageContent } from "./not-found-page-content.component"
 
-const renderNotFountContent = () => {
-  const { container } = render(<NotFoundPageContent />)
-  return {
-    get title() {
-      return container.querySelector("h1")
-    },
-    get description() {
-      return container.querySelector("p")
-    },
-    get link() {
-      return container.querySelector("[href='/']")
-    },
-  }
-}
-
 describe("Content of the Not Found page", () => {
+  beforeEach(() => render(<NotFoundPageContent />))
   describe("given a user lands on the 404 page", () => {
     describe("when he wants to know what happened", () => {
-      it("shows the information about the content not being found", () => {
-        const { title } = renderNotFountContent()
-        expect(title).not.toBeEmptyDOMElement()
+      it("should show the information about the content not being found", () => {
+        expect(screen.getByRole("heading")).not.toBeEmptyDOMElement()
       })
       it("shows some cheesy explanation", () => {
-        const { description } = renderNotFountContent()
-        expect(description).not.toBeEmptyDOMElement()
+        expect(screen.getByText(/not able to find/)).toBeInTheDocument()
       })
     })
     describe("when he wants to go back to safety", () => {
-      it("allows him to navigate to the home page", () => {
-        const { link } = renderNotFountContent()
-        expect(link).toLinkTo("/")
+      it("should allow him to navigate to the home page", () => {
+        expect(screen.getByRole("link")).toLinkTo("/")
       })
     })
   })
