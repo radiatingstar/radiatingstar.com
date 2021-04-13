70a4b1b4769086ab3c5a88d31a7c20f6271f0752
Mateusz Kocz <mateusz@kocz.pl>
2021-04-13 20:58:42 +0200

    Set up jest axe

--- src/testing/setup-after-env.ts
+++ src/testing/setup-after-env.ts
@@ -3,9 +3,12 @@ import "./matchers/array-to-contain-object.matcher"
 import "./matchers/to-link-to.matcher"
 import "./config/jest-dom"
 import "@relmify/jest-fp-ts"
+import { toHaveNoViolations } from "jest-axe"
 
 // Read more about what's that here:
 // https://github.com/facebook/jest/issues/335#issuecomment-485382254
 jest.mock("../seo/ui/seo/seo.component", () =>
   jest.requireActual("../seo/ui/seo/__mocks__/seo.component")
 )
+
+expect.extend(toHaveNoViolations)
