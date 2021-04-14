9e2d92d4486460d58df0e868c53a920c9c299ddc
Mateusz Kocz <mateusz@kocz.pl>
2021-04-14 21:30:10 +0200

    Style the projects page

--- src/projects/ui/projects-page/projects-page.test.tsx
+++ src/projects/ui/projects-page/projects-page.test.tsx
@@ -7,7 +7,7 @@ import { ProjectsPage } from "./projects-page"
 describe("Projects Page component", () => {
   checkAccessibility(<ProjectsPage layout={TestLayout} />)
   beforeEach(() => render(<ProjectsPage layout={TestLayout} />))
-  it("should set the page title", () => {
+  it("should set the page title X", () => {
     expect(screen.getByText("[title] Projects")).toBeInTheDocument()
   })
   it("should display the main heading", () => {
@@ -21,41 +21,52 @@ describe("Projects Page component", () => {
       "http://mateuszkocz.github.io/3l/",
       "https://github.com/mateuszkocz/3l",
       "../../assets/lots-of-love-for-less.png",
+      "Lots of Love for Less Description",
     ],
     [
       "Mail Generator",
       "https://mateuszkocz.github.io/mail-generator/",
       "https://github.com/mateuszkocz/mail-generator",
       "../../assets/mail-generator.png",
+      "Mail Generator Description",
     ],
-  ])("for %s project", (projectName, projectLink, repoLink, imageSource) => {
-    let box: ReturnType<typeof within>
-    beforeEach(
-      () =>
-        (box = within(
-          screen.getByRole("heading", { name: projectName })
-            ?.parentNode as HTMLElement
-        ))
-    )
-    it("should display the title", () => {
-      expect(
-        screen.getByRole("heading", { name: projectName, level: 2 })
-      ).toBeInTheDocument()
-    })
-    it("should display the link to the project", () => {
-      // eslint-disable-next-line testing-library/prefer-screen-queries
-      const link = box.getByRole("link", { name: /visit the project/i })
-      expect(link).toLinkTo(projectLink)
-    })
-    it("should display the link to the project's repo", () => {
-      // eslint-disable-next-line testing-library/prefer-screen-queries
-      const link = box.getByRole("link", { name: /repository/i })
-      expect(link).toLinkTo(repoLink)
-    })
-    it("should display the image", () => {
-      // eslint-disable-next-line testing-library/prefer-screen-queries
-      const image = box.getByRole("img")
-      expect(image).toHaveAttribute("src", imageSource)
-    })
-  })
+  ])(
+    "for %s project",
+    (projectName, projectLink, repoLink, imageSource, description) => {
+      let box: ReturnType<typeof within>
+      beforeEach(
+        () =>
+          // FIXME: Looks bad. So many .parentNote.
+          (box = within(
+            screen.getByRole("heading", { name: projectName })?.parentNode
+              ?.parentNode as HTMLElement
+          ))
+      )
+      it("should display the title", () => {
+        expect(
+          screen.getByRole("heading", { name: projectName, level: 2 })
+        ).toBeInTheDocument()
+      })
+      it("should display the link to the project", () => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const link = box.getByRole("link", { name: /visit the project/i })
+        expect(link).toLinkTo(projectLink)
+      })
+      it("should display the link to the project's repo", () => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const link = box.getByRole("link", { name: /repository/i })
+        expect(link).toLinkTo(repoLink)
+      })
+      it("should display the image", () => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const image = box.getByRole("img")
+        expect(image).toHaveAttribute("src", imageSource)
+      })
+      // TODO
+      it.skip("should display the project's description", () => {
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(box.getByText(description)).toBeInTheDocument()
+      })
+    }
+  )
 })
--- src/projects/ui/projects-page/projects-page.tsx
+++ src/projects/ui/projects-page/projects-page.tsx
@@ -1,51 +1,99 @@
+import { StaticImage } from "gatsby-plugin-image"
 import React, {
   JSXElementConstructor,
   PropsWithChildren,
   VoidFunctionComponent,
 } from "react"
 import styled from "styled-components"
-import { CoreLayout, ExternalLink, List } from "../../../backbone"
+import {
+  CoreLayout,
+  ExternalLink,
+  List,
+  ListComponentProperties,
+} from "../../../backbone"
 import { SEO } from "../../../seo"
-import { StaticImage } from "gatsby-plugin-image"
 
-const projects = [
+interface ProjectInfo {
+  name: string
+  projectLink: string
+  repoLink: string
+  description: string
+}
+
+const projectsList: ProjectInfo[] = [
   {
     name: "Lots of Love for Less",
     projectLink: "http://mateuszkocz.github.io/3l/",
     repoLink: "https://github.com/mateuszkocz/3l",
-    image: <StaticImage src="../../assets/lots-of-love-for-less.png" alt="" />,
+    description: `
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Animi commodi dolorum minus nobis. At dicta exercitationem
+                  itaque perferendis quam, sapiente sequi soluta veniam.
+                  Adipisci deserunt error itaque modi necessitatibus voluptas.
+                `,
   },
   {
     name: "Mail Generator",
     projectLink: "https://mateuszkocz.github.io/mail-generator/",
     repoLink: "https://github.com/mateuszkocz/mail-generator",
-    image: <StaticImage src="../../assets/mail-generator.png" alt="" />,
+    description: `
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Animi commodi dolorum minus nobis. At dicta exercitationem
+                  itaque perferendis quam, sapiente sequi soluta veniam.
+                  Adipisci deserunt error itaque modi necessitatibus voluptas.
+                `,
   },
 ]
 
-type Properties = {
+type Properties = { projects: ProjectInfo[] } & {
   layout?: JSXElementConstructor<PropsWithChildren<unknown>>
 }
 
 export const ProjectsPage: VoidFunctionComponent<Properties> = ({
+  projects = projectsList,
   layout: Layout = CoreLayout,
 }) => {
   return (
     <Layout>
       <SEO title="Projects" />
-      <h1>Projects</h1>
-      <List
+      <PageTitle>Projects</PageTitle>
+      <ProjectsList<ListComponentProperties<ProjectInfo>>
         renderItem={(project) => {
+          const image =
+            project.name === "Lots of Love for Less" ? (
+              <StaticImage
+                src="../../assets/lots-of-love-for-less.png"
+                alt=""
+                style={{
+                  height: "100%",
+                  width: "100%",
+                }}
+              />
+            ) : (
+              <StaticImage
+                src="../../assets/mail-generator.png"
+                alt=""
+                style={{
+                  height: "100%",
+                  width: "100%",
+                }}
+              />
+            )
           return (
             <Project>
-              {project.image}
-              <ProjectTitle as="h2">{project.name}</ProjectTitle>
-              <ProjectExternalLink href={project.projectLink}>
-                Visit the project
-              </ProjectExternalLink>
-              <ProjectExternalLink href={project.repoLink}>
-                Repository
-              </ProjectExternalLink>
+              <Image>{image}</Image>
+              <ProjectContent>
+                <ProjectTitle as="h2">{project.name}</ProjectTitle>
+                <Description>{project.description}</Description>
+                <Footer>
+                  <ProjectExternalLink href={project.projectLink}>
+                    Visit the project
+                  </ProjectExternalLink>
+                  <ProjectExternalLink href={project.repoLink}>
+                    Repository
+                  </ProjectExternalLink>
+                </Footer>
+              </ProjectContent>
             </Project>
           )
         }}
@@ -55,19 +103,74 @@ export const ProjectsPage: VoidFunctionComponent<Properties> = ({
   )
 }
 
+const PageTitle = styled.h1`
+  margin: 0 auto 4rem;
+  color: var(--yellow-500);
+  font-size: 4rem;
+  text-align: center;
+`
+
+const ProjectsList = styled(List)`
+  display: grid;
+  align-items: stretch;
+  margin: 0 2rem 6rem;
+  gap: 4rem;
+  grid-auto-rows: 1fr;
+  grid-template-columns: 1fr;
+  justify-items: stretch;
+`
+
 const Project = styled.section`
-  padding: 2rem;
-  margin-bottom: 2rem;
+  display: flex;
+  overflow: hidden;
+  height: 100%;
+  flex-direction: column;
   background: white;
   border-radius: 4px;
+
+  @media (min-width: 48rem) {
+    flex-direction: row;
+
+    & > * {
+      width: 50%;
+    }
+  }
+`
+
+const Image = styled.div`
+  height: 100%;
+  line-height: 0;
+`
+
+const ProjectContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+`
+
+const Description = styled.p`
+  margin: 0 0 2rem;
+  line-height: 1.6;
+`
+
+const Footer = styled.footer`
+  margin-top: auto;
 `
 
 const ProjectExternalLink = styled(ExternalLink)`
-  color: var(--yellow-400);
+  display: inline-block;
+  padding: 1rem;
+  background: var(--yellow-700);
+  border-radius: 4px;
+  color: white;
   text-decoration: none;
+
+  & + & {
+    margin-left: 1rem;
+  }
 `
 
 const ProjectTitle = styled.h1`
-  margin-top: 0;
+  margin: 0 0 2rem;
   color: var(--black-300);
 `
