1106d2da370ab42af3872307dd7cccbd03e052cf
Mateusz Kocz <mateusz@kocz.pl>
2021-04-17 22:56:01 +0200

    Change the logo colurs

--- src/branding/ui/logo/logo.component.tsx
+++ src/branding/ui/logo/logo.component.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components"
 
 export const Image = styled.div.attrs({ role: "img", "aria-hidden": true })`
   --logo-size: 4rem;
+  --primary-logo-color: var(--red-100);
+  --secondary-logo-color: var(--yellow-700);
 
   display: flex;
   width: var(--logo-size);
@@ -12,18 +14,21 @@ export const Image = styled.div.attrs({ role: "img", "aria-hidden": true })`
 
   background-image: repeating-conic-gradient(
     from 0deg,
-    #ffa500 0deg 30deg,
-    #268bd2 30deg 60deg,
-    #ffa500 60deg 90deg,
-    #268bd2 90deg 120deg
+    var(--secondary-logo-color) 0deg 30deg,
+    var(--primary-logo-color) 30deg 60deg,
+    var(--secondary-logo-color) 60deg 90deg,
+    var(--primary-logo-color) 90deg 120deg
   );
   border-radius: 10px;
+  box-shadow: 5px 5px 0 var(--secondary-logo-color),
+    -5px 5px 0 var(--secondary-logo-color),
+    -5px -5px var(--secondary-logo-color), 5px -5px var(--secondary-logo-color);
 `
 
 export const Glyph = styled.span`
   color: white;
   font-size: calc(var(--logo-size) - 1.5rem);
-  opacity: 0.8;
+  opacity: 0.9;
   text-shadow: 0 0 10px rgb(255 255 255), 0 0 10px rgb(255 255 255);
 `
 
