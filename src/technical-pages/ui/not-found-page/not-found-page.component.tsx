75ac78a93c51aa43b35b673b3409c450613d82a9
Mateusz Kocz <mateusz@kocz.pl>
2021-05-04 20:00:19 +0200

    Properly style the 404 page

--- src/technical-pages/ui/not-found-page/not-found-page.component.tsx
+++ src/technical-pages/ui/not-found-page/not-found-page.component.tsx
@@ -1,10 +1,72 @@
+import { Link } from "gatsby"
 import React, { FunctionComponent } from "react"
+import styled from "styled-components"
+import {
+  ContentBlock,
+  CoreLayout,
+  PageTitle,
+  WithLayout,
+} from "../../../backbone"
 import { SEO } from "../../../seo"
-import { NotFoundPageContent } from "../not-found-page-content/not-found-page-content.component"
 
-export const NotFoundPage: FunctionComponent = () => (
-  <div>
+export const NotFoundPage: FunctionComponent<WithLayout<unknown>> = ({
+  layout: Layout = CoreLayout,
+}) => (
+  <Layout>
     <SEO title="404: Not Found" />
-    <NotFoundPageContent />
-  </div>
+    <Content>
+      <PageTitle>There are so many starts in the universe!</PageTitle>
+      <NotFoundInfo attentionGrabbing>
+        <p>No wonder we were not able to find this one. </p>
+        <p>
+          <PageLink to="/blog">Visit the blog</PageLink> or{" "}
+          <PageLink to="/projects">take a look at the projects</PageLink>.
+        </p>
+      </NotFoundInfo>
+    </Content>
+  </Layout>
 )
+
+const Content = styled.section`
+  margin: 0 2rem;
+
+  ${PageTitle} {
+    margin-top: 0;
+  }
+`
+
+const NotFoundInfo = styled(ContentBlock)`
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+
+  p {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  p + p {
+    margin-top: 1rem;
+  }
+`
+
+const PageLink = styled(Link)`
+  border-bottom: 1px solid currentColor;
+  color: var(--red-100);
+  text-decoration: none;
+
+  &:hover,
+  &:active,
+  &:focus {
+    color: var(--yellow-700);
+  }
+
+  @media (prefers-color-scheme: dark) {
+    color: var(--yellow-700);
+
+    &:hover,
+    &:active,
+    &:focus {
+      color: var(--red-100);
+    }
+  }
+`
--- src/technical-pages/ui/not-found-page/not-found-page.test.tsx
+++ src/technical-pages/ui/not-found-page/not-found-page.test.tsx
@@ -1,13 +1,32 @@
 import { render, screen } from "@testing-library/react"
 import React from "react"
+import { TestLayout } from "../../../testing/components/test-layout.component"
 import { NotFoundPage } from "./not-found-page.component"
 
 describe("Not Found page", () => {
+  beforeEach(() => render(<NotFoundPage layout={TestLayout} />))
   describe("given a user lands on the Not Found Page", () => {
     describe("when he looks at the browser's title bar", () => {
       it("shows him the 404 title", async () => {
-        render(<NotFoundPage />)
         expect(screen.getByText("[title] 404: Not Found")).toBeInTheDocument()
+      })
+    })
+    describe("when he wants to know what happened", () => {
+      it("should show the information about the content not being found", () => {
+        expect(screen.getByRole("heading")).not.toBeEmptyDOMElement()
+      })
+      it("shows some cheesy explanation", () => {
+        expect(screen.getByText(/not able to find/)).toBeInTheDocument()
+      })
+    })
+    describe("when he wants to go back to safety", () => {
+      it("should allow him to navigate to the blog page", () => {
+        expect(screen.getByRole("link", { name: /blog/i })).toLinkTo("/blog")
+      })
+      it("should allow him to navigate to the projects page", () => {
+        expect(screen.getByRole("link", { name: /projects/i })).toLinkTo(
+          "/projects"
+        )
       })
     })
   })
