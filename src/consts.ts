429e3fc90a84f023fcb734e0cb012247f4ec8406
Mateusz Kocz <mateusz@kocz.pl>
2023-08-20 15:54:09 +0200

    Add proer values for the rss plugin

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Radiating Star";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Doing the web things with curiosity.";
